2a839a6f070e2c936f65c2fdea5fe4b86d4f4bef
mironshoh1603 <mironshohasadov1603@gmail.com>
2023-05-15 01:56:55 +0500

    added signup route

---
+++ src/users/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+  async signUp(email: string, password: string) {
+    const users = await this.userService.find(email);
+    if (users.length) {
+      return new BadRequestException('Your email is allready used!');
+    }
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+  }
+  signIn() {}
+}
